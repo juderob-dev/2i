686b048c3aecd7b19d87fc07d4c55b72658c1bb5
juderob-dev <juderoberts09@gmail.com>
2022-10-15 12:57:39 +0100

    update with exceptions

--- code.py
+++ code.py
@@ -12,10 +12,29 @@ def remove_duplicates(num_arr:list, num_dup: int)->list:
             i+=1
     return sorted(num_arr, reverse=True)
 
+
+def test_type(arr, type):
+    assert all(isinstance(x, type) for x in arr)
+
+
+
+def test_length(arr, spec_len):
+    assert len(arr) == spec_len
+
+
+def test_range(arr, min_r, max_r):
+    assert min(arr) >= min_r and max(arr) <= max_r
+
+
 if __name__=="__main__":
     arr=[1,1,2,2,3,4,5,5,100,4]
+    min_r = 1
+    max_r = 100
+    length =10
+    test_range(arr,1, 100)
+    test_length(arr,length)
+    test_type(arr, int)
     print (remove_duplicates(arr,4))
-
 
 
 
